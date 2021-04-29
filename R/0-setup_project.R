287c80812035b877da0235892bb266efb7fd3dc1
shannonjarvis9 <shannonjarvis@cmail.carleton.ca>
2021-04-29 12:46:00 -0400

    Modified the setwd

--- R/0-setup_project.R
+++ R/0-setup_project.R
@@ -1,6 +1,8 @@
-#setup the package 
+#setup the package and working directories 
+
 wd <- list()
-setwd("/home/shannon/NAPS_project/NAPS_wrangling/")
+setwd('~/NAPS_project/NAPS_wrangling/')
+
 wd$data   <- paste0(getwd(),"/data/")
 wd$output   <- paste0(getwd(),"/output/")
 wd$header   <- paste0(getwd(),"/header_files/")
