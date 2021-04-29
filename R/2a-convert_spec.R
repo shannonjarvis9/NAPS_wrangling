fddee4f944dfb8a04ffbdad7e623f1b9a538839f
shannonjarvis9 <shannonjarvis@cmail.carleton.ca>
2021-04-29 16:11:39 -0400

    Reviewed code, added more informative comments, created rename_df function (was prev. repeated code)

--- R/2a-convert_spec.R
+++ R/2a-convert_spec.R
@@ -1,7 +1,31 @@
+# 2a-convert_spec.R
+# In this script, the list of spec data is converted to the appropriate format 
+# for merging in subsequent scripts 
+
+# Manual fixes are preformed, field blanks are removed, columns are renamed using
+# cartridges and using header dictionary 
+
+
+# Setup/load the necessary files 
+# ------------------------------------------------------------------------------
+
+source('~/NAPS_project/NAPS_wrangling/R/0-setup_project.R') # setup the working dir
+
 load(paste0(wd$output, "spec_read.rda"))
 
+# Load the header dictionary (sheets correspond to names of file types in the spec data)
+# The header dictionary was manually created, its format is as follows;
+  # A sheet for each file type (i.e. CARB, IC, NH4...)
+  # Within each sheet, 1st row: column names in the pre 2010 data
+  #                    2nd row: corresponding col names for the 2010+ data
+  #                    3rd row: new file type name (may be missing for some - that's okay!)
+  #                    4th row: column type (i.e. numeric, char...) not used, may be missing 
+filename <-paste0(wd$header, "spec_header.xlsx")
+sheets <- getSheetNames(filename)
+spec_header_dict <- lapply(sheets, read.xlsx, xlsxFile = filename)
+names(spec_header_dict) <- sheets
 
-
+##----------------------------------------------------------------------------
 # Need to make some manual fixes to the spec data - certain stations had 
 # formatting issues 
 ##----------------------------------------------------------------------------
@@ -18,8 +42,8 @@ for(s in c("S70301", "S105001", "S106600")){
   spec[[s]][["WICPMS"]] <- spec[[s]][["WICPMS"]] %>% 
     mutate(mass_ug_m3 = mass) %>%
     select(-"mass")
-  
 }
+
 
 spec[["S100702"]][["ICPMS"]] <- spec[["S100702"]][["ICPMS"]]%>% 
   select(-starts_with('na'))
@@ -64,15 +88,16 @@ spec <- lapply(spec, lapply, function(x){if(length(x) != 0){
 
 
 # Dealing with duplicate dates from each data frame
+# Renaming CARB using cartridge, SPEC using media
 # ICPMS, WICPMS has fine and coarse data 
-# --------------------------------------------------------------------
 ##----------------------------------------------------------------------------
-#try making this code nicer 
 icpms_coarse <- vector("list", length(spec))
 names(icpms_coarse) <- names(spec)
 wicpms_coarse <- icpms_coarse
 
+
 for(i in 1:length(spec)){
+  
   # Rename carb using cartridge 
   if(length(spec[[i]][["CARB"]] ) != 0){
     spec[[i]][["CARB"]] <-  pivot_wider(spec[[i]][["CARB"]], names_from = "cart", 
@@ -88,17 +113,18 @@ for(i in 1:length(spec)){
                                       names_glue = "media_{media}_{.value}", names_prefix = "media_",
                                       values_from = names(spec[[i]][["IC"]])[7:ncol(spec[[i]][["IC"]])])
   }
+  if(length(field_blanks[[i]][["IC"]] ) != 0){
+    field_blanks[[i]][["IC"]] <-  pivot_wider(field_blanks[[i]][["IC"]], names_from = "media", 
+                                              names_glue = "media_{media}_{.value}", names_prefix = "media_",
+                                              values_from = names(field_blanks[[i]][["IC"]])[7:ncol(field_blanks[[i]][["IC"]])])
+  }
   
-  # ICPMS has fine and coarse data
+  
+  # ICPMS has fine, coarse and T data
   if(length(spec[[i]][["ICPMS"]] ) != 0){
     icpms_coarse[[i]] <- spec[[i]][["ICPMS"]] %>% 
       filter(fraction == "C") %>% 
       select(-"fraction")
-    
-    # icpms_total[[i]] <- spec[[i]][["ICPMS"]] %>% 
-    #   filter(fraction == "T") %>% 
-    #   select(-"fraction")
-    
     
     spec[[i]][["ICPMS"]] <- spec[[i]][["ICPMS"]] %>% 
       filter(fraction == "F") %>% 
@@ -118,9 +144,12 @@ for(i in 1:length(spec)){
   }
 }
 
+
+
 # clean up the lists 
 wicpms_coarse <- list.clean(wicpms_coarse, fun = function(x) nrow(x) == 0L || length(x) == 0L) 
 icpms_coarse <- list.clean(icpms_coarse, fun = function(x) nrow(x) == 0L || length(x) == 0L) 
+
 
 # Remove the cartridge/media columns - all important info from these cols has been used
 spec <- lapply(spec, lapply, function(x){if(length(x) != 0){
@@ -129,23 +158,37 @@ spec <- lapply(spec, lapply, function(x){if(length(x) != 0){
   else if("media" %in% names(x)){x %>% select(-media)} 
   else {x}}})
 
+field_blanks <- lapply(field_blanks, lapply, function(x){if(length(x) != 0){
+  if("cart" %in% names(x)){x %>%  select(-cart)}
+  else if("cartridge" %in% names(x)){x %>% select(-cartridge)} 
+  else if("media" %in% names(x)){x %>% select(-media)} 
+  else {x}}})
 
 
 ##----------------------------------------------------------------------------
 # Now spec has one observation per date 
 # It is organized as spec -> station -> file type 
+
+# Lets rename so it can be merged with the 2010+ data
 ##----------------------------------------------------------------------------
 
 
-## Now lets rename spec 
+
+
+
+##----------------------------------------------------------------------------
+##----------------------------------------------------------------------------
+## Rename and reformat spec using the header dictionary/conversion file 
+##----------------------------------------------------------------------------
+##----------------------------------------------------------------------------
 
 ## Function: get_names
 ##---------------------------------
 ## Inputs: col_name: column name to match with 2010+ col names 
-##         pmpart_conv: df with the conversion structure 
-## Outputs: vector containing the new column names and the new file type
+##         conversion_df: df with the conversion structure 
+## Outputs: the new column names if it exists
 get_names <- function(col_name, conversion_df) {
-  col <- which(col_name == conversion_df[1, ])
+  col <- which(col_name == conversion_df[1,])
   
   if ((is.integer(col) &&
        length(col) == 0L) | is.na(conversion_df[2, col])) {
@@ -156,42 +199,45 @@ get_names <- function(col_name, conversion_df) {
 }
 
 
-##----------------------------------------------------------------------------
-##----------------------------------------------------------------------------
-## Rename and reformat spec using the header dictionary/conversion file 
-##----------------------------------------------------------------------------
-##----------------------------------------------------------------------------
-
-filename <-paste0(wd$header, "spec_header.xlsx")
-sheets <- getSheetNames(filename)
-spec_header_dict <- lapply(sheets, read.xlsx, xlsxFile = filename)
-names(spec_header_dict) <- sheets
-
-
-# can replace loops with lapply 
-for(s in names(spec)){
-  for(t in names(spec[[s]])){
-    
-    if(length(spec[[s]][[t]]) != 0){
-        for(i in 1:ncol(spec[[s]][[t]])){
-          names(spec[[s]][[t]])[i] <- get_names(names(spec[[s]][[t]])[i], spec_header_dict[[t]])
-      
+## Function: rename_df
+##---------------------------------
+## Inputs: df- list of list whose names are being converted
+##         header_dict- name conversion scheme, a list
+## Process: iterates through the input list (df) to rename each column and list
+##          to match the format of the header dictionary 
+rename_df <- function(df, header_dict) {
+  for (s in names(df)) {
+    for (t in names(df[[s]])) {
+      if (length(df[[s]][[t]]) != 0) {
+        for (i in 1:ncol(df[[s]][[t]])) {
+          names(df[[s]][[t]])[i] <-
+            get_names(names(df[[s]][[t]])[i], header_dict[[t]])
+        }
       }
+      type_idx <- which(names(df[[s]]) == t)
+      names(df[[s]])[[type_idx]] <-
+        na.omit(unique(as.character(header_dict[[t]][3, ])))
     }
-    type_idx <- which(names(spec[[s]]) == t)
-    names(spec[[s]])[[type_idx]] <- na.omit(unique(as.character(spec_header_dict[[t]][3,])))
   }
+  df
 }
 
 
+
+
+spec <- rename_df(spec, spec_header_dict)
+field_blanks <- rename_df(field_blanks, spec_header_dict)
+
+
 ##----------------------------------------------------------------------------
 ##----------------------------------------------------------------------------
-## Combine, rename and reformat the coarse data  
+## Create, combine, rename and reformat the coarse data  
 ##----------------------------------------------------------------------------
 ##----------------------------------------------------------------------------
+# Create a new list to hold the data (will only contain WICPMS, ICPMS)
 spec_coarse <- vector("list")
 
-# add wicpms
+# add wicpms coarse to the coarse spec data 
 for(s in names(wicpms_coarse)){
     for(i in 1:ncol(wicpms_coarse[[s]])){
       names(wicpms_coarse[[s]])[i] <- get_names(names(wicpms_coarse[[s]])[i], spec_header_dict[["WICPMS"]])
@@ -216,3 +262,7 @@ for(s in names(icpms_coarse)){
 ##----------------------------------------------------------------------------
 save(file = paste0(wd$output, "spec_2010_format.rda"), spec)
 save(file = paste0(wd$output, "spec_coarse_2010_format.rda"), spec_coarse)
+save(file = paste0(wd$output, "spec_fieldblanks_2010_format.rda"), field_blanks)
+
+
+
